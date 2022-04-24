9c67ed3505e9bcaf6736446a0d1e7a1fbfc17c7d
Naz M <nazlee41@gmail.com>
2022-04-25 01:26:58 +0800

    ci: implement commitlint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
